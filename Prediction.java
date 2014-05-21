7a0ff041b6f329c3efae322ae9a325e49e69a60e
maggywy <maggywy@gmail.com>
2014-05-20 19:04:10 -0700

    Create Prediction.java

---
+++ Prediction.java
@@ -0,0 +1,12 @@
+/*Prediction.java
+ * 
+ * This is the main class of the project that calls the calculation class to calculate the close price
+ * of April for Lennar Cor, San Disk, and QualComm stock and execute the predict close price to csv file. 
+ */
+public class Prediction {
+	
+	public static void main(String[] args) {
+				
+		Calculation calc = new Calculation();
+	}
+}
